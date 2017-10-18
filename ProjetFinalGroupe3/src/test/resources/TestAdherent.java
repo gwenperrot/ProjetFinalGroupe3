a2d643cd25444876a7c91d82fca478e2fc6559e4
Doria <doria.voisin@gmail.com>
2017-10-18 16:52:46 +0200

    coucou c'est les tests unitaires

---
+++ ProjetFinalGroupe3/src/test/resources/TestAdherent.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAdherent {
+
+	@Test
+	public void testUpdateAdherent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEmprunter() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ ProjetFinalGroupe3/src/test/resources/TestAdmin.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestAdmin {
+
+
+
+	@Test
+	public void testCreateAdherent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRechercheParOeuvre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAttribuerLivreOeuvre() {
+		fail("Not yet implemented");
+	}
+
+}
