f4020013dd4318ea87b4a9bd1eea3dada2c1fe3e
mgordino <gordino.miguel@gmail.com>
2017-10-19 16:28:06 +0200

    emprunter fonctionel

--- ProjetFinalGroupe3/src/main/java/com/Societe/ProjetFinalGroupe3/metier/Oeuvre.java
+++ ProjetFinalGroupe3/src/main/java/com/Societe/ProjetFinalGroupe3/metier/Oeuvre.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -36,7 +37,7 @@ private int nbLivreLibre;
 private List<OeuvreAdherent> oeuvreAdherent = new ArrayList<OeuvreAdherent>();
 
 @JsonIgnore
-@OneToMany(mappedBy="loeuvre")
+@OneToMany(mappedBy="loeuvre", fetch=FetchType.EAGER)
 private Set<Livre> livres = new HashSet<Livre>();
 
 
